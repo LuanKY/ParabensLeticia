fb390ddc0bafc8a6c1b46e821900c9d4b3420075
LuanKY <83087980+LuanKY@users.noreply.github.com>
2025-01-24 17:03:57 -0300

    Add files via upload

--- effect.js
+++ effect.js
@@ -25,7 +25,7 @@ $('document').ready(function(){
 		$('#bulb_orange').addClass('bulb-glow-orange');
 		$('body').addClass('peach');
 		$(this).fadeOut('slow').delay(5000).promise().done(function(){
-			$('#play').fadeIn('slow');
+			$('#play').click();
 		});
 	});
 	$('#play').click(function(){
@@ -39,15 +39,15 @@ $('document').ready(function(){
 		$('#bulb_orange').addClass('bulb-glow-orange-after');
 		$('body').css('backgroud-color','#FFF');
 		$('body').addClass('peach-after');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
-			$('#bannar_coming').fadeIn('slow');
+		$(this).fadeOut('slow').delay(7000).promise().done(function(){
+			$('#bannar_coming').click();
 		});
 	});
 
 	$('#bannar_coming').click(function(){
 		$('.bannar').addClass('bannar-come');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
-			$('#balloons_flying').fadeIn('slow');
+		$(this).fadeOut('slow').delay(7000).promise().done(function(){
+			$('#balloons_flying').click();
 		});
 	});
 
@@ -119,22 +119,22 @@ $('document').ready(function(){
 		loopSix();
 		loopSeven();
 		
-		$(this).fadeOut('slow').delay(5000).promise().done(function(){
-			$('#cake_fadein').fadeIn('slow');
+		$(this).fadeOut('slow').delay(6000).promise().done(function(){
+			$('#cake_fadein').click();
 		});
 	});	
 
 	$('#cake_fadein').click(function(){
 		$('.cake').fadeIn('slow');
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
-			$('#light_candle').fadeIn('slow');
+		$(this).fadeOut('slow').delay(4000).promise().done(function(){
+			$('#light_candle').click();
 		});
 	});
 
 	$('#light_candle').click(function(){
 		$('.fuego').fadeIn('slow');
 		$(this).fadeOut('slow').promise().done(function(){
-			$('#wish_message').fadeIn('slow');
+			$('#wish_message').click();
 		});
 	});
 
@@ -158,44 +158,39 @@ $('document').ready(function(){
 		$('#b66').animate({top:240, left: vw+150},500);
 		$('#b77').animate({top:240, left: vw+250},500);
 		$('.balloons').css('opacity','0.9');
-		$('.balloons h2').fadeIn(3000);
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
+		$('.balloons h2').fadeIn(4000);
+		$(this).fadeOut('slow').delay(4000).promise().done(function(){
 			$('#story').fadeIn('slow');
 		});
 	});
 	
-	$('#story').click(function(){
+	$('#story').click(function () {
 		$(this).fadeOut('slow');
-		$('.cake').fadeOut('fast').promise().done(function(){
+		$('.cake').fadeOut('fast').promise().done(function () {
 			$('.message').fadeIn('slow');
 		});
-		
+	
+		// Abrir a página wish.html em uma nova aba
+		window.open('./mensagem/wish.html', '_blank');
+	
 		var i;
-
-		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
-			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
-				});
-				
-			}
-			else{
-				msgLoop(i);
-			}			
-
-		});
-			// body...
+	
+		function msgLoop(i) {
+			$("p:nth-child(" + i + ")").fadeOut('slow').delay(800).promise().done(function () {
+				i = i + 1;
+				$("p:nth-child(" + i + ")").fadeIn('slow').delay(1000);
+				if (i == 50) {
+					$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+						$('.cake').fadeIn('fast');
+					});
+	
+				} else {
+					msgLoop(i);
+				}
+			});
 		}
-		
+	
 		msgLoop(0);
-		
 	});
+	
 });
-
-
-
-
-//alert('hello');
